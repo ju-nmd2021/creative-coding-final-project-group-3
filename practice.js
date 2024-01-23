5c3eab36d6e42b769ae496e98f6328f952a1136c
Chandler Abel <93584687+abch21li@users.noreply.github.com>
2024-01-23 10:49:28 +0100

    particles draw lines

--- practice.js
+++ practice.js
@@ -5,16 +5,30 @@ canvas.height=window.innerHeight;
 
 ctx.fillStyle = 'white';
 ctx.strokeStyle = 'white';
-ctx.lineWidth = 10;
+ctx.lineWidth = 1;
 
 class Particle {
     constructor(effect) {
           this.effect = effect;
           this.x = Math.floor(Math.random() * this.effect.width);
           this.y = Math.floor(Math.random() * this.effect.height);
+          this.speedX = Math.random() * 0.5 - 0.1;
+          this.speedY = Math.random() * 0.5 - 0.1;
+          this.history = [{x: this.x, y: this.y}];
         }
           draw(context) {
                context.fillRect(this.x, this.y, 1, 1);
+               context.beginPath();
+               context.moveTo(this.history[0].x, this.history[0].y);
+               for (let i = 0; i < this.history.length; i++){
+                context.lineTo(this.history[i].x, this.history[i].y);
+               }
+               context.stroke();
+          }
+          update(){
+            this.x += this.speedX;
+            this.y += this.speedY;
+            this.history.push({x: this.x, y: this.y});
           }
     }
 
@@ -24,21 +38,31 @@ class Effect {
         this.height = height;
         this.particles = [];
         this.numberOfParticles = 2000;
-        effect.init();
+
     }
     init(){
         for (let i = 0; i < this.numberOfParticles; i++){
             this.particles.push(new Particle(this));
         }
-        
     }
     render(context){
         this.particles.forEach(particle => {
             particle.draw(context);
+            particle.update();
         })
     }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
 
-effect.render(ctx);
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    effect.render(ctx);
+    requestAnimationFrame(animate);
+    
+    
+}
+
+animate();
+effect.init();
+
