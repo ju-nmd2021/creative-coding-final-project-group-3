748746f5c4141365fbcec8a4e0a18004a1f367d8
Chandler Abel <93584687+abch21li@users.noreply.github.com>
2024-01-25 11:43:26 +0100

    Added update function

--- particle.js
+++ particle.js
@@ -7,3 +7,11 @@ class Particle(){
      this.prevPos = this.pos.copy();
     }
 }
+
+update() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+}
+
