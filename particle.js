74353815904165a50e36955f9e9d73a70d7f87a0
Chandler Abel <93584687+abch21li@users.noreply.github.com>
2024-01-25 11:49:20 +0100

    added show function

--- particle.js
+++ particle.js
@@ -1,4 +1,4 @@
-class Particle(){
+class Particle() {
     constructor(){
      this.pos = createVector(random(width), random(height));
      this.vel = createVector(0, 0);
@@ -6,14 +6,13 @@ class Particle(){
      this.maxspeed = 4;
      this.prevPos = this.pos.copy();
     }
-}
 
 update() {
     this.vel.add(this.acc);
     this.vel.limit(this.maxspeed);
     this.pos.add(this.vel);
     this.acc.mult(0);
-}
+};
 
 follow(vectors) {
     let x = floor(this.pos.x / scl);
@@ -21,9 +20,15 @@ follow(vectors) {
     let index = x + y * cols;
     let force = vectors[index];
     this.applyForce(force);
-}
+};
 
 applyForce(force) {
     this.acc.add(force);
 }
 
+show() {
+    stroke(255, 10);
+    strokeWeight(1);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    this.updatePrev();
+}
