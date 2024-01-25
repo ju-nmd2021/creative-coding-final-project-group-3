743e248c90e88c63b737bc38bb0ffad4cb6d62fc
Chandler Abel <93584687+abch21li@users.noreply.github.com>
2024-01-25 11:41:36 +0100

    Particle set up

--- particle.js
+++ particle.js
@@ -1,5 +1,9 @@
 class Particle(){
     constructor(){
-
+     this.pos = createVector(random(width), random(height));
+     this.vel = createVector(0, 0);
+     this.acc = createVector(0, 0);
+     this.maxspeed = 4;
+     this.prevPos = this.pos.copy();
     }
 }
