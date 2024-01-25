5354972f8895915dbd5e438c1a94d78930f43df9
Chandler Abel <93584687+abch21li@users.noreply.github.com>
2024-01-25 12:19:18 +0100

    Added flowfield generation to existing random shape functions / removed switch functions

--- script.js
+++ script.js
@@ -201,46 +201,78 @@ function randomPopShape() {
     // let modulatedColor 
     // let modulated
 
-    switch (rndSongInt) {
-        case 1: 
-       
-        break;
-        case 2: 
-        
-        break;
-        case 3: 
-    
-        break;
-        case 4: 
-       
-        break;
-        case 5: 
-       
-    break;
+    var yoff = 0;
+
+    for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[index] = v;
+      xoff += inc;
+      stroke(0, 50);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // strokeWeight(1);
+      // line(0, 0, scl, 0);
+      // pop();
     }
+      yoff += inc;
+
+      zoff += 0.0003;
+    }
+
+    for (var i = 0; i < particles.length; i++) {
+      particles[i].follow(flowfield);
+      particles[i].update();
+      particles[i].edges();
+      particles[i].show();
+    }
+
+  //  fr.html(floor(frameRate()));
+    
 }
 
 //----------Defines the waveform shapes, colors and responses for the 'rock' genre 
 function randomRockShape() {
 
-    
-    switch (rndSongInt) {
-        case 1: 
-        
-        break; 
-        case 2: 
-        
-        break;
-        case 3: 
-       
-        break;
-        case 4: 
-       
-        break;
-        case 5: 
-       
-    break;
-     }
+    var yoff = 0;
+
+    for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      flowfield[index] = v;
+      xoff += inc;
+      stroke(0, 50);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // strokeWeight(1);
+      // line(0, 0, scl, 0);
+      // pop();
+      }
+
+      yoff += inc;
+
+      zoff += 0.0003;
+    }
+
+    for (var i = 0; i < particles.length; i++) {
+      particles[i].follow(flowfield);
+      particles[i].update();
+      particles[i].edges();
+      particles[i].show();
+    }
+
+  //  fr.html(floor(frameRate()));
+   
     }
  
 //------------------------------------------\\BUTTONS//-----------------------------//
